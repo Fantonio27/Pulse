cdfb53058f9d7941f4dd3220d21f6440403e33ea
Francis <francis.antonio0927@gmail.com>
2026-01-24 20:22:33 +0800

    feat: add landing components barrel export

---
+++ src/components/landing/index.ts
@@ -0,0 +1,7 @@
+export { default as Header } from "./Header";
+export { default as HeroSection } from "./HeroSection";
+export { default as RecentlyUsed } from "./RecentlyUsed";
+export { default as SearchFilter } from "./SearchFilter";
+export { default as DailyDrivers } from "./DailyDrivers";
+export { default as AllTools } from "./AllTools";
+export { default as Footer } from "./Footer";
