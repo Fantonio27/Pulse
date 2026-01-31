20b9ef58f7b7b90023179b39253657b8c4d31935
Francis <francis.antonio0927@gmail.com>
2026-01-31 18:40:05 +0800

    chore: remove an unspecified file.

--- src/components/landing/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Header } from "./Header";
-export { default as HeroSection } from "./HeroSection";
-export { default as RecentlyUsed } from "./RecentlyUsed";
-export { default as SearchFilter } from "./SearchFilter";
-export { default as DailyDrivers } from "./DailyDrivers";
-export { default as AllTools } from "./AllTools";
-export { default as Footer } from "./Footer";
